8698ce813cce4ca6ad455351eee1bc15b874ea03
tidyoux <313247557@qq.com>
2017-11-03 19:05:59 +0800

    add global contract script.

---
+++ tests/contract/global.lua
@@ -0,0 +1,47 @@
+
+--[[
+global 合约。
+--]]
+
+local L0 = require("L0")
+
+function L0Init(args)
+    return true
+end
+
+function L0Invoke(funcName, args)
+    if type(args) ~= "table" then
+        return false
+    end
+
+    local key = args[0]
+    if type(key) ~= "string" then
+        return false
+    end
+
+    if funcName == "SetGlobalState" then
+        local value = args[1]
+        if not(value) then
+            return false
+        end
+        L0.SetGlobalState(key, value)
+        return true
+    else if funcName == "DelGlobalState" then
+        L0.DelGlobalState(key)
+        return true
+    end
+    return false
+end
+
+function L0Query(args)
+    if type(args) ~= "table" then
+        return ""
+    end
+
+    local key = args[0]
+    if type(key) ~= "string" then
+        return ""
+    end
+
+    return L0.GetGlobalState(key)
+end
